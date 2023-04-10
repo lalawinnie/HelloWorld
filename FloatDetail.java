cd68ec7086282966e53511eaa33f50ee01ebf767
lalawinnie <wenwen.jiang0604@gmail.com>
2023-04-11 02:26:18 +0900

    java practise

---
+++ FloatDetail.java
@@ -0,0 +1,15 @@
+public class FloatDetail {
+	public static void main(String[] args) { 
+		float num1 = 1.1F;
+		double num2 = 1.1;
+		double num3 = 1.1F;
+		
+		double num4 = .123;
+		System.out.println(num4);
+		System.out.println(5.12e2);
+		System.out.println(5.12E-2);
+		
+		
+	}
+}
+	
---
+++ VarDetail.java
@@ -0,0 +1,20 @@
+public class VarDetail {
+	public static void main(String[] args) {
+		int a = 50;
+		System.out.println(a);
+		//a = "jack";//错误
+		a = 88;
+		System.out.println(a);
+		
+		// int a = 77; //错误
+		
+
+	}
+}
+
+class Cat {
+	public static void main(String[] args) {
+		int a = 666;
+		System.out.println(a);
+	}
+}
