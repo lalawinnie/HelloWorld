a08a15ee9d78093048f69ee4e18a20ba56995202
lalawinnie <wenwen.jiang0604@gmail.com>
2023-04-18 00:05:51 +0800

    java practise

---
+++ ArithmeticOperatorExercise02.java
@@ -0,0 +1,15 @@
+
+public class ArithmeticOperatorExercise02{
+	public static void main(String[] args) { 
+		int days = 59;
+		int weeks = days / 7;
+		int leftDays = days % 7;
+		System.out.println(days + "天 合" + weeks + "星期零" + leftDays + "天");
+		
+		
+		double huaShi =  234.6;
+		double sheShi = 5.0 / 9 * (huaShi - 100);
+		System.out.println("华氏温度" + huaShi + "对应的摄氏温度=" + sheShi);
+	}
+}
+	
